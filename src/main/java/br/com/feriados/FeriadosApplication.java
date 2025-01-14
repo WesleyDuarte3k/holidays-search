40062d0ca060e156524bd5d476e31b19ab4a8f3d
Wesley Ribeiro <wesleyduarteribeiro013@gmail.com>
2025-01-14 20:36:11 -0300

    :hammer: Adding Feign

--- src/main/java/br/com/feriados/FeriadosApplication.java
+++ src/main/java/br/com/feriados/FeriadosApplication.java
@@ -1,11 +1,16 @@
 package br.com.feriados;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 import java.util.TimeZone;
 
+@EnableFeignClients
 @SpringBootApplication
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class FeriadosApplication {
 
 	public static void main(String[] args) {
