77aa9dbae437c9b9a86a79aa884b4dbcad2e65dd
Wesley Ribeiro <wesleyduarteribeiro013@gmail.com>
2025-01-15 02:56:13 -0300

    :hammer: Creating convertHolidays in HolidayResource

--- src/main/java/br/com/feriados/infrastructure/resource/HolidayResource.java
+++ src/main/java/br/com/feriados/infrastructure/resource/HolidayResource.java
@@ -1,6 +1,7 @@
 package br.com.feriados.infrastructure.resource;
 
 import br.com.feriados.domain.entity.Holiday;
+import br.com.feriados.domain.entity.dto.HolidayDTO;
 import br.com.feriados.domain.service.HolidayService;
 import br.com.feriados.infrastructure.database.HolidayRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,65 +25,95 @@ public class HolidayResource {
 	private HolidayRepository holidayRepository;
 
 	@RequestMapping("/holidays")
-	public ResponseEntity <List<Holiday>> findHoliday (@RequestParam(required = false) String city, @RequestParam(required = false) String date,
-	                                  @RequestParam(required = false) String cep) {
+	public ResponseEntity<List<HolidayDTO>> findHoliday (
+		@RequestParam(required = false) String city,
+	  @RequestParam(required = false) String date,
+		@RequestParam(required = false) String cep,
+		@RequestParam(required = false) String state
+	) {
 		if(city != null) {
-			return ResponseEntity.ok(holidayService.findAllByCityName(city));
+			try {
+				return ResponseEntity.ok(findAllByCity(city));
+
+			} catch (RuntimeException e) {
+				ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			}
 		}
 		else if (date != null) {
-			return findAllByDate(date);
+			try {
+				return ResponseEntity.ok(findAllByDate(date));
+
+			} catch (RuntimeException e) {
+				ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			}
 		}
 		else if (cep != null) {
-			return findCep(cep);
+			try {
+				return ResponseEntity.ok(findCep(cep));
+			} catch (RuntimeException e) {
+				ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			}
+		}
+		else if (state != null) {
+			try {
+				return ResponseEntity.ok(findAllStateHolidays(state));
+
+			} catch (RuntimeException e) {
+				ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			}
 		}
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 
 
 
-
-	@RequestMapping("/find-all")
-	public List<Holiday> findAll() {
-		return holidayRepository.findAll();
-	}
-
-//	@GetMapping("/find-all-by-city")
-	public ResponseEntity <List<Holiday>> findAllByCity(String cityName) {
-		try {
-			List<Holiday> holidays = holidayService.findAllByCityName(cityName);
-			return ResponseEntity.ok(holidays);
-		} catch (RuntimeException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-		}
-	}
-
-	@GetMapping("/find-all-by-date")
-	public ResponseEntity <List<Holiday>> findAllByDate(String date) {
-		List<Holiday> holidays = holidayService.findAllByDate(date);
-		return ResponseEntity.ok(holidays);
-	}
-
-	@GetMapping("/find-national-holidays")
-	public ResponseEntity<List<Holiday>> findNationalHolidays() {
-		List<Holiday> holidays = holidayService.findAllNationalHolidays();
-		return ResponseEntity.ok(holidays);
-	}
-
-	@GetMapping("/find-all-by-city")
-	public ResponseEntity<List<Holiday>> findCep(@RequestParam("cep") String cep) {
-		try {
-			List<Holiday> holidays = holidayService.findAllByCep(cep);
-			return ResponseEntity.ok(holidays);
-		} catch (RuntimeException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-		}
-	}
-
-
-//	@GetMapping("/find-all-state-holidays")
-//	public ResponseEntity<List<Holiday>> findAllStateHolidays (@RequestParam("state") String state) {
-//		List<Holiday> holidays = holidayService.findAllStateHolidays(state);
-//		return ResponseEntity.ok(holidays);
 //
+//	@RequestMapping("/find-all")
+//	public List<Holiday> findAll() {
+//		return holidayRepository.findAll();
 //	}
+//
+	public List<HolidayDTO>findAllByCity (String cityName) {
+		List<Holiday> holidays = holidayService.findAllByCityName(cityName);
+		List<HolidayDTO> holidaysDTOS = convertHolidays(holidays);
+		return holidaysDTOS;
+	}
+//
+	public List<HolidayDTO> findAllByDate(String date) {
+		List<Holiday> holidays = holidayService.findAllByDate(date);
+		List<HolidayDTO> holidaysDTOS = convertHolidays(holidays);
+		return holidaysDTOS;
+	}
+//
+//	@GetMapping("/find-national-holidays")
+//	public ResponseEntity<List<Holiday>> findNationalHolidays() {
+//		List<Holiday> holidays = holidayService.findAllNationalHolidays();
+//		return ResponseEntity.ok(holidays);
+//	}
+//
+	public List<HolidayDTO> findCep(String cep) {
+			List<Holiday> holidays = holidayService.findAllByCep(cep);
+			List<HolidayDTO> holidaysDTOS = convertHolidays(holidays);
+			return (holidaysDTOS);
+	}
+
+	public List<HolidayDTO> findAllStateHolidays (String state) {
+			List<Holiday> holidays = holidayService.findAllbyState(state);
+			List<HolidayDTO> holidaysDTOS = convertHolidays(holidays);
+			return holidaysDTOS;
+	}
+
+	public List<HolidayDTO> convertHolidays(List<Holiday> holidays) {
+		List<HolidayDTO> holidaysDTOS = new ArrayList<>();
+		for (Holiday holiday : holidays) {
+			HolidayDTO holidayDTO = new HolidayDTO();
+			holidayDTO.setDate(holiday.getDate());
+			holidayDTO.setName(holiday.getName());
+			holidayDTO.setType(holiday.getType().name());
+			holidayDTO.setState(holiday.getState().getName());
+
+			holidaysDTOS.add(holidayDTO);
+		}
+		return holidaysDTOS;
+	}
 }
