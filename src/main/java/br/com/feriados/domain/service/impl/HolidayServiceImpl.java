9cb1da3e6d70e4a2ed3d9e25c9065e8f46b3fce1
Wesley Ribeiro <wesleyduarteribeiro013@gmail.com>
2025-01-15 02:53:01 -0300

    :hammer: Adding findAllByState in HolidayServiceImpl

--- src/main/java/br/com/feriados/domain/service/impl/HolidayServiceImpl.java
+++ src/main/java/br/com/feriados/domain/service/impl/HolidayServiceImpl.java
@@ -3,18 +3,25 @@ package br.com.feriados.domain.service.impl;
 import br.com.feriados.domain.entity.City;
 import br.com.feriados.domain.entity.Holiday;
 import br.com.feriados.domain.entity.HolidayType;
+import br.com.feriados.domain.entity.State;
+import br.com.feriados.domain.entity.dto.HolidayDTO;
 import br.com.feriados.domain.service.HolidayService;
 import br.com.feriados.infrastructure.database.CityRepository;
 import br.com.feriados.infrastructure.database.HolidayRepository;
+import br.com.feriados.infrastructure.database.StateRepository;
 import br.com.feriados.infrastructure.exception.InvalidCityException;
 import br.com.feriados.infrastructure.exception.NotFoundCityException;
 import br.com.feriados.infrastructure.exception.NotFoundHolidayException;
+import br.com.feriados.infrastructure.exception.NotFoundStateException;
 import br.com.feriados.infrastructure.viaCep.Address;
 import br.com.feriados.infrastructure.viaCep.ViaCepClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 
@@ -27,6 +34,8 @@ public class HolidayServiceImpl implements HolidayService {
 	private CityRepository cityRepository;
 	@Autowired
 	private ViaCepClient viaCepClient;
+	@Autowired
+	private StateRepository stateRepository;
 
 
 	@Override
@@ -36,9 +45,11 @@ public class HolidayServiceImpl implements HolidayService {
 
 	@Override
 	public List<Holiday> findAllByType(Enum holidayType) {
-		return holidayRepository.findAll().stream()
+		List<Holiday> holidays = holidayRepository.findAll().stream()
 			.filter(holiday -> holiday.getType().equals(holidayType))
 			.collect(Collectors.toList());
+
+		return holidays;
 	}
 
 	@Override
@@ -48,38 +59,24 @@ public class HolidayServiceImpl implements HolidayService {
 			throw new InvalidCityException();
 		}
 
-
 		City city = cityRepository.findByName(cityName);
 		if (Objects.isNull(city)) {
 			throw new NotFoundCityException();
 		}
-
+		Optional<State> state = stateRepository.findById(city.getState().getId());
 		List<Holiday> holidays = holidayRepository.findAllByCityId(city.getId());
 		holidays.addAll(findAllByType(HolidayType.NATIONAL));
+		holidays.addAll(findAllbyState(state.get().getName()));
 
 		return holidays;
 	}
 
-	@Override
-	public List<Holiday> holidayFilter(String Type) {
-		if (Type.equals("NATIONAL")) {
-			return holidayRepository.findAllByType(Type);
-		}
-		else if (Type.equals("REGIONAL")) {
-			return holidayRepository.findAllByType(Type);
-		}
-		else if (Type.equals("LOCAL")) {
-			return holidayRepository.findAllByType(Type);
-		}
-		else {
-			throw new InvalidCityException();
-		}
-	}
 
 	@Override
 	public List<Holiday> findAllByDate(String date) {
 		if (date != null) {
 			List<Holiday> holidays = holidayRepository.findAllByDate(date);
+
 			return holidays;
 		}
 		throw new NotFoundHolidayException();
@@ -90,17 +87,21 @@ public class HolidayServiceImpl implements HolidayService {
 		return findAllByType(HolidayType.NATIONAL);
 	}
 
-//	@Override
-//	public List<Holiday> findAllStateHolidays(String state) {
-//		if (state != null) {
-//			State currentState = stateRepository.findByName(state);
-//
-//			if (currentState != null) {
-//
-//			}
-//		}
-//
-//	}
+	@Override
+	public List<Holiday> findAllbyState(String stateName) {
+		if (Objects.isNull(stateName)) {
+			throw new NotFoundStateException();
+		}
+
+		State state = stateRepository.findByName(stateName);
+		if (Objects.isNull(state)) {
+			throw new NotFoundStateException();
+		}
+
+		return holidayRepository.findAllByStateId(state.getId()).stream()
+			.filter(holiday -> holiday.getType().equals(HolidayType.STATE))
+			.collect(Collectors.toList());
+	}
 
 	@Override
 	public List<Holiday> findAllByCep(String cep) {
@@ -109,4 +110,6 @@ public class HolidayServiceImpl implements HolidayService {
 
 		return holidays;
 	}
+
+
 }
